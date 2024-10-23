29da0b275cc5bebf470d196501bf50b7f384d951
sunwoo <summerham22@gmail.com>
2024-10-23 17:02:28 +0900

    jpa

--- src/main/java/shop/samdule/demo/entity/TodoEntity.java
+++ src/main/java/shop/samdule/demo/entity/TodoEntity.java
@@ -4,12 +4,23 @@ import lombok.Setter;
 import lombok.Getter;
 import lombok.ToString;
 
+import org.checkerframework.checker.signature.qual.Identifier;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todo_jpa")
 @Setter
 @Getter
 @ToString
 public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false)
     private String subject;
+    @Column(nullable = false)
     private String body;
     private Boolean completed;
 }
